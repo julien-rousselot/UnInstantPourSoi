0d2056e1ae7f088b45e59c934ce3e4cd84d9e46e
Julien Rousselot <julienrousselot16@gmail.com>
2024-06-05 22:08:03 +0200

    meilleure presentation

--- instantpourtoi/src/component/Contact/Contact.tsx
+++ instantpourtoi/src/component/Contact/Contact.tsx
@@ -7,15 +7,7 @@ function Contact () {
 
     return(
         <main>
-    <section>
-        <img className="imageHeader" src={imageHeader} alt="" />
-        <div className="description">
-            <img className="mainPhoto" src={photoMain} alt="" />
-            <h2>Accordez-vous un instant de détente</h2>
-            <p>Découvrez un univers reposant, calme, élégant et épuré. Un espace de bien être pour vous inviter au lâcher-prise. Diverses prestations esthétique vous sont proposées, tel que des soins du visage et des massages sur mesure en accord avec vos envies et vos désirs, un soin innovant avec la technologie Neocare pour diminuer les signes de l'âge et la cellulite, des épilations, la beauté des ongles, le rehaussement de cils pour sublimer votre regard, les maquillages notamment le maquillage mariée ect.</p>
-        </div>
-        <Message />
-    </section>
+   <h1>cobt</h1>
     </main>
         )
 }
--- instantpourtoi/src/component/Header/Header.tsx
+++ instantpourtoi/src/component/Header/Header.tsx
@@ -1,5 +1,5 @@
 import './Header.scss';
-import logoLoup from '../../assets/images/loup-removebg-preview.png';
+import logo from '../../assets/images/logo.jpg';
 import { NavLink } from 'react-router-dom';
 
 interface HeaderProps{
@@ -10,9 +10,7 @@ function Header ({ isVisible, toggleVisibility }:HeaderProps) {
     return(
         <header>
        <nav id="navigation">
-                <a href="#" className="logo-container">
-                    <img src={logoLoup} alt="logo un instant pour soi" />
-                </a>
+                <NavLink  to="/"><img src={logo} alt="logo un instant pour soi" /></NavLink>
                 <div className={`menu-icon ${isVisible ? 'is-opened' : ''}`}>
                     <span onClick={toggleVisibility}>
                     </span>
@@ -28,32 +26,32 @@ function Header ({ isVisible, toggleVisibility }:HeaderProps) {
                 </div>
                 <ul className={`navList ${isVisible ? 'active' : ''}`}>
                     <li>
-                        <NavLink to="/">Accueil</NavLink>
+                        <NavLink className={'nav-link'} to="/">Accueil</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/Apropos">A propos</NavLink>
+                        <NavLink className={'nav-link'} to="/Apropos">A propos</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/Prestations">Prestations</NavLink>
+                        <NavLink className={'nav-link'}  to="/Prestations">Prestations</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/Tarifs">Tarifs</NavLink>
+                        <NavLink className={'nav-link'} to="/Tarifs">Tarifs</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/Contact">Contact</NavLink>
+                        <NavLink className={'nav-link'} to="/Contact">Contact</NavLink>
                     </li>
                 </ul>
             </nav>
 
             <nav className="navigationLaptop">
-                <img src={logoLoup} alt="logo un instant pour soi" />
+                <img src={logo} alt="logo un instant pour soi" />
                 <ul className="navListLaptop">
-                    <li><NavLink to="/">Accueil</NavLink></li>
-                    <li><NavLink to="/Apropos">À propos</NavLink></li>
-                    <li><NavLink to="/Prestations">Prestations</NavLink></li>
-                    <li><NavLink to="/Tarifs">Tarifs</NavLink></li>
-                    <li><NavLink to="/Contact">Contact</NavLink></li>
-                </ul>
+                    <li><NavLink className={`nav-link`} to="/">Accueil</NavLink></li>
+                    <li><NavLink className={`nav-link`} to="/Apropos">À propos</NavLink></li>
+                    <li><NavLink className={`nav-link`} to="/Prestations">Prestations</NavLink></li>
+                    <li><NavLink className={`nav-link`} to="/Tarifs">Tarifs</NavLink></li>
+                    <li><NavLink className={`nav-link`} to="/Contact">Contact</NavLink></li>
+                    </ul>
             </nav>
         </header>
     )
